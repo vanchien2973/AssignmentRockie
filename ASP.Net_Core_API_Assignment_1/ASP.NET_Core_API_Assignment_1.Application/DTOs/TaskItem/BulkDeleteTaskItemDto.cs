efc229810dabcc52a0e6cc3aa4ec9adb644fdfcc
Thai Van Chien <vanchien2973@gmail.com>
2025-04-08 21:06:08 +0700

    Delete ASP.Net_Core_API_Assignment_1 directory

--- ASP.Net_Core_API_Assignment_1/ASP.NET_Core_API_Assignment_1.Application/DTOs/TaskItem/BulkDeleteTaskItemDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace ASP.NET_Core_API_Assignment_1.Application.DTOs;
-
-public class BulkDeleteTaskItemDto
-{
-    [MinLength(1, ErrorMessage = "At least one task ID is required")]
-    public List<Guid> TaskIds { get; set; }
-}
